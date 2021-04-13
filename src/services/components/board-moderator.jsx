634f31a2ca7b4aadade14a6dc7f2597d85575239
Curt Grogan <curt.grogan@fdmgroup.com>
2021-04-13 10:51:53 -0400

    Added components. moderator & admin pages remain.

---
+++ src/services/components/board-moderator.jsx
@@ -0,0 +1,9 @@
+// Will be similar to BoardUser page
+
+// Role-based Pages
+// We’re gonna have 3 pages for accessing protected data:
+
+// BoardUser page calls UserService.getUserBoard()
+// BoardModerator page calls UserService.getModeratorBoard()
+// BoardAdmin page calls UserService.getAdminBoard()
+// I will show you User Page for example, other Pages are similar to this Page.
